f38b5213703f068e8923b73d42cfc4d0de610a14
Muhammet <muhammetzsahin@gmail.com>
2018-03-08 15:27:18 +0300

    Remove url and api keys

--- BraintreeSample/BraintreeSample/GlobalSettings.cs
+++ BraintreeSample/BraintreeSample/GlobalSettings.cs
@@ -5,7 +5,7 @@ namespace BraintreeSample
   {
     public static readonly string MERCHANT_ACCOUNT_ID = "fake_switch_usd";
 
-    public static readonly string BASE_SERVER_URL = "http://braintreesamplewebapi20180304062013.azurewebsites.net";
+    public static readonly string BASE_SERVER_URL = "http://YOUR_WEB_API_URL";
 
   }
 }
